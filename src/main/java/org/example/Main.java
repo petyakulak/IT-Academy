3ddbfc7c73c67658386a3f3aa68a6a85c2fe4871
Petr Kulakov <petyakulak@very-soft.com>
2025-05-06 22:15:55 +0700

    + zadachi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,10 +23,49 @@ class Conversion {
 
         System.out.println("Введите сумму в долларах: ");
         double dollar = console.nextDouble();
-        double map = 0.85;
+        double rate = 0.85;
 
-        double convertedEuro = dollar * map;
+        double convertedEuro = dollar * rate;
         System.out.println("Сумма в евро: " + convertedEuro);
     }
 }
 
+class Maximum {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+
+        System.out.println("Введите 10 целых чисел: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+             int chisla = console.nextInt();
+             numbers[i] = chisla * chisla;
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Максимальное число: " + max);
+    }
+}
+
+class ComingAge {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Введите свой возраст: ");
+        int age = console.nextInt();
+
+        if (age < 18) {
+            System.out.println("Вы не совершеннолетний!");
+        } else {
+            System.out.println("Вы совершеннолетний!");
+        }
+    }
+}
