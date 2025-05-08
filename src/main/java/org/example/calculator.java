080935cedcd40599232aa9b8c2bb3e18c2084b55
Petr Kulakov <petyakulak@very-soft.com>
2025-05-08 20:32:30 +0700

    + calculator

---
+++ src/main/java/org/example/calculator.java
@@ -0,0 +1,48 @@
+package org.example;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        String otvet = "да";
+        while (otvet.equalsIgnoreCase("да")) {
+            Scanner console = new Scanner(System.in);
+
+            System.out.println("Введите первое число: ");
+            int chislo1 = console.nextInt();
+            System.out.println("Введите второе число: ");
+            int chislo2 = console.nextInt();
+
+            console.nextLine(); //очистка буфера
+            System.out.println("Введите операцию (+,-,*,/): ");
+            String oper = console.nextLine();
+
+            switch (oper) {
+                case "+":
+                    System.out.println(chislo1 + chislo2);
+                    break;
+                case "-":
+                    System.out.println(chislo1 - chislo2);
+                    break;
+                case "*":
+                    System.out.println(chislo1 * chislo2);
+                    break;
+                case "/":
+                    if (chislo2 != 0) {
+                        System.out.println(chislo1 / chislo2);
+                        break;
+                    } else {
+                        System.out.println("Ошибка! Деление на ноль!");
+                        break;
+                    }
+                default:
+                    System.out.println("Неверная операция!");
+                    break;
+            }
+
+            System.out.println("Хотите ли вы продолжить? (да/нет): ");
+            String otvet1 = console.nextLine();
+            otvet = otvet1;
+        }
+    }
+}
